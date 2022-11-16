62b5f34a11ca388273f4c7c4e44b7e700859b0a9
ajaharleo <ajaharuddin10217@gmail.com>
2022-11-17 03:41:12 +0530

    testing heroku deployment2

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
     install_requires = get_requirements_list()
 )
