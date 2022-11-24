43cec0e5fc8f8e5fbd24fa0f163c651ad5e1d989
ajaharleo <ajaharuddin10217@gmail.com>
2022-11-25 00:41:56 +0530

    Final application tested

--- src/BigMartSales/entity/__init__.py
+++ src/BigMartSales/entity/__init__.py
@@ -1,3 +1,4 @@
 from BigMartSales.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig
 from BigMartSales.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact,ModelPusherArtifact
 from BigMartSales.entity.model_factory import *
+from BigMartSales.entity.sales_predictor import salesPredictor, SalesData
